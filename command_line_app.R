89c2d7d8a3fbd71bbf3722ff595b7ab88dc6c9ad
Maximilian Kasy (laptop) <maxkasy@gmail.com>
2019-03-10 08:09:29 -0700

    Create command_line_app.R

---
+++ command_line_app.R
@@ -0,0 +1,18 @@
+source("ReadDataApp.R")
+source("MCMC_HierarchicalThompson.R")
+
+RR=10000
+alpha=0.2
+
+datapath="priordata_test_missings.csv"
+  
+priordata=ReadDataApp(datapath)
+
+Pstar=DtchoiceMCMCProbabilities(priordata$Y,priordata$D,priordata$X, #outcomes, treatments, and covariates thus far
+                                priordata$k,priordata$nx, #number of treatments and number of strata
+                                RR)
+
+Pactual=(1-alpha) * Pstar + alpha*(1/priordata$k)
+
+filename = paste(Sys.Date(), "treatmentprobabilities.csv", sep="")
+write_csv(Pactual, path=filename)
