3bdead8396b6b99cfdac565afb8aeba088eadb83
Maximilian Kasy (laptop) <maxkasy@gmail.com>
2019-03-10 08:15:47 -0700

    hardcode nx=16

--- command_line_app.R
+++ command_line_app.R
@@ -7,12 +7,13 @@ alpha=0.2
 datapath="priordata_test_missings.csv"
   
 priordata=ReadDataApp(datapath)
+priordata$nx=16
 
 Pstar=DtchoiceMCMCProbabilities(priordata$Y,priordata$D,priordata$X, #outcomes, treatments, and covariates thus far
                                 priordata$k,priordata$nx, #number of treatments and number of strata
                                 RR)
 
-Pactual=(1-alpha) * Pstar + alpha*(1/priordata$k)
+Pactual=(1-alpha) * Pstar + alpha * (1/priordata$k)
 
 filename = paste(Sys.Date(), "treatmentprobabilities.csv", sep="")
 write_csv(Pactual, path=filename)
